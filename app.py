a81293c2ae99c675ebec7ed930d03c32634d9e38
CM <52335952+danieletchie@users.noreply.github.com>
2021-11-13 13:38:56 +0100

    Changed the database uri to postgresql

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ load_dotenv()
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://data.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
